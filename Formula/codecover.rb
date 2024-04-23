1a33a0ddeec3526327c4a8810a32fc33abc5bf04
Release Workflow <release@workflow.com>
2024-04-23 12:59:20 +0000

    Update formula to 1.0.6

--- Formula/codecover.rb
+++ Formula/codecover.rb
@@ -1,8 +1,8 @@
 class Codecover < Formula
     desc "Generate code coverage report in `lcov` format for multiplatform projects"
     homepage "https://github.com/ciuliene/codecover"
-    url "https://github.com/ciuliene/homebrew-codecover/releases/download/1.0.5/codecover-v1.0.5.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    url "https://github.com/ciuliene/homebrew-codecover/releases/download/1.0.6/codecover-v1.0.6.tar.gz"
+    sha256 "24f5e30089ec391f9bdf672d0c9f4c8c529763d926b6c1e7ec3f56dedb3a8de3"
     license "MIT"
 
     def install
@@ -10,6 +10,6 @@ class Codecover < Formula
     end
 
     test do
-        assert_match "v1.0.5", shell_output("#{bin}/codecover --version")
+        assert_match "v1.0.6", shell_output("#{bin}/codecover --version")
     end
 end
